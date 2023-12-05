a40c3ad66493fe5dcb618e317593a2d46da8cf6f
Zowie Erickson <zowie.erickson@gmail.com>
2023-12-05 18:53:39 -0500

    Update image path for buystrip

--- src/components/shared/BuyStrip.jsx
+++ src/components/shared/BuyStrip.jsx
@@ -5,21 +5,21 @@ export default function BuyStrip() {
         <section className="buystrip-section">
             <div className="buystrip">
                 <div className="buystrip-item headphones">
-                    <img src="../../../src/assets/shared/desktop/image-category-thumbnail-headphones.png" alt="" />
+                    <img src={"../../../src/assets/shared/desktop/image-category-thumbnail-headphones.png"} alt="" />
                     <div className="buystrip-item-copy">
                         <h3 className="buystrip-item-title">headphones</h3>
                         <a className="buystrip-item-cta" href="">shop</a>
                     </div>
                 </div>
                 <div className="buystrip-item headphones">
-                    <img src="../../../src/assets/shared/desktop/image-category-thumbnail-headphones.png" alt="" />
+                    <img src={"../../../src/assets/shared/desktop/image-category-thumbnail-headphones.png"} alt="" />
                     <div className="buystrip-item-copy">
                         <h3 className="buystrip-item-title">headphones</h3>
                         <a className="buystrip-item-cta" href="">shop</a>
                     </div>
                 </div>
                 <div className="buystrip-item headphones">
-                    <img src="../../../src/assets/shared/desktop/image-category-thumbnail-headphones.png" alt="" />
+                    <img src={"../../../src/assets/shared/desktop/image-category-thumbnail-headphones.png"} alt="" />
                     <div className="buystrip-item-copy">
                         <h3 className="buystrip-item-title">headphones</h3>
                         <a className="buystrip-item-cta" href="">shop</a>
